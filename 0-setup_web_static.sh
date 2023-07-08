028c22cf29c01342d2ddb5ab0ae1f63d4c1e18a9
Njokinjoroge <zeejaymorgan@gmail.com>
2023-07-08 07:24:59 +0000

    morgan

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,32 @@
-#!/usr/bin/python3
-from fabric.api import local
-from time import strftime
-from datetime import date
+0-setup_web_static.sh
+apt-get update
+apt-get install -y nginx
 
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-def do_pack():
-    """ A script that generates archive the contents of web_static folder"""
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-    filename = strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
-
-        return "versions/web_static_{}.tgz".format(filename)
-
-    except Exception as e:
-        return None
+service nginx restart
